29e95713290e8845ddac392915b2a48ca454de7a
AutomationTesting0612 <automationtesting0612@gmail.com>
2023-07-27 07:43:11 +0530

    spring mail

---
+++ src/main/java/com/spring/mail/spring/mail/Application.java
@@ -0,0 +1,25 @@
+package com.spring.mail.spring.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class Application {
+
+	@Autowired
+	private EmailSenderService emailSenderService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+    public void sendMail() {
+		emailSenderService.sendEmail("akhil.sharma0612@gmail.com", "FirstEmail", "Test");
+	}
+
+
+}
